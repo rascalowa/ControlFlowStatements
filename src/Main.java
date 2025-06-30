8e717d0317aa29fd40a991fbeaa4882cfcce9b7e
Monika Sikora <monikastanko90@gmail.com>
2025-06-30 14:21:11 +0200

    ControlFlowStatements

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        int switchValue = 2;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Value was 3, 4 or 5");
+                break;
+            default:
+                System.out.println("Value was different than 1, 2, 3, 4 or 5");
+                break;
+        }
+
+        String month = "XYZ";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+        switch (month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+        return "BAD";
+    }
+}
